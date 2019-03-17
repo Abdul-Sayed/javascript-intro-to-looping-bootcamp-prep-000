b53e4c934d5d156fcc7c5493591371f33773d916
Abdul <awsayed@alaska.edu>
2019-03-17 23:17:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,7 @@
 function forLoop(arr) {
   for (let i = 1; i < 26; i++) {
-  // ...
+    if (i === 1) {
+      arr.push(`I am ${i} strange loop.`)
+    } else arr.push(`I am ${i} strange loops.`)
   }
 }
